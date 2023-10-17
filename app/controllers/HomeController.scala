76441e6c8a8f98969477afd62789d2e21ecd16bf
jonaboecker <jona.boecker@web.de>
2023-10-17 16:23:23 +0200

    first commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,56 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+import de.htwg.lovecraftletter.controller.controllerImpl._
+import de.htwg.lovecraftletter.model.GameStateImpl.GameState
+import de.htwg.lovecraftletter.controller.controllState
+import de.htwg.lovecraftletter.aview._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+    val gameController = new Controller(GameState(0, Nil, Nil, 0), (controllState.standard, ""), -999)
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+  def health() = Action {
+    Ok("ok")
+  }
+  def board() = Action {
+    Ok(gameController.handle)
+  }
+  def runLL() = Action {
+    gameController.runLL
+    Ok("Game started")
+  }
+  def getPlayerAmount(player: Int) = Action {
+    gameController.playerAmount(player)
+    Ok("Game started with " + player + " players")
+  }
+  def setPlayerName(name: String) = Action {
+    gameController.playerName(name)
+    Ok("Player name set to " + name)
+  }
+  def playCard(card: Int) = Action {
+    gameController.setVarUserInput(card)
+    gameController.makeTurn
+    Ok("Card " + card + " played")
+  }
+  def selectPlayer(player: Int) = Action {
+    gameController.playerChoosed(player)
+    Ok("Player " + player + " selected")
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,21 @@
+name := """lovecraft_letter_web"""
+organization := "com.example"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.13.12"
+
+scalacOptions += "-Ytasty-reader"
+
+libraryDependencies += guice
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "6.0.0-RC2" % Test
+unmanagedBase := baseDirectory.value / "lib"
+
+
+// Adds additional packages into Twirl
+//TwirlKeys.templateImports += "com.example.controllers._"
+
+// Adds additional packages into conf/routes
+// play.sbt.routes.RoutesKeys.routesImport += "com.example.binders._"
